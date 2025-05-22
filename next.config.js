a5cb14387f29961b2c62540e310a9feaf92bdfad
Joshua <joshualai9922@gmail.com>
2025-05-22 22:16:58 +0800

    add postinstall

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = {
+  allowedDevOrigins: [
+    "local-origin.dev",
+    "*.local-origin.dev",
+    "192.168.18.62",
+  ],
+};
