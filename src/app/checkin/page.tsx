62739e040e369f233190fe00b1194d64dee7a6b0
Joshua <joshualai9922@gmail.com>
2025-05-26 11:42:32 +0800

    mobile height fix

--- src/app/checkin/page.tsx
+++ src/app/checkin/page.tsx
@@ -2,7 +2,7 @@ import { GroupAssigner } from "@/components/GroupAssigner";
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full justify-center px-6 pt-6 pb-10 md:p-10">
+    <div className="flex w-full justify-center px-6 pt-6 pb-10 md:p-10">
       <div className="w-full max-w-sm">
         <GroupAssigner />
       </div>
