3fa6d401a814f02e6d0da06207f673acd60e15a1
Joshua <joshualai9922@gmail.com>
2025-06-05 11:13:50 +0800

    add biz unit mapping table

---
+++ prisma/migrations/20250605031240_add_business_unit_mapping/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "BusinessUnitMapping" (
+    "email" TEXT NOT NULL,
+    "businessUnit" TEXT NOT NULL,
+
+    CONSTRAINT "BusinessUnitMapping_pkey" PRIMARY KEY ("email")
+);
