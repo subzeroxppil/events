3914ddb63f1cb5defe8afdd93698b092200715dc
Joshua <joshualai9922@gmail.com>
2025-07-31 23:28:49 +0800

    update landing ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,82 @@
+// "use client";
+// import { Button } from "@/components/ui/button";
+// import { Card } from "@/components/ui/card";
+// import Image from "next/image";
+// import { House, ShieldUser } from "lucide-react";
+// import Link from "next/link";
+
+// export default function Page() {
+//   return (
+//     <div className="flex w-full justify-center p-6 md:p-10 h-full">
+//       <div className="w-full max-w-sm">
+//         <div className="flex flex-col gap-4">
+//           <Card className="mx-auto w-full max-w-sm p-6">
+//             <div className="flex flex-col items-center text-center">
+//               {/* <House size={40} /> */}
+//               <Image
+//                 src="/paypal_logo.png"
+//                 width={70}
+//                 height={35}
+//                 alt="paypal icon"
+//               />
+//               <p className="mb-2 text-2xl font-bold">Events</p>
+//               <div className="w-full flex flex-col gap-2 mt-4">
+//                 <Link href="/admin">
+//                   <Button size={"lg"} className="w-full" variant="outline">
+//                     <ShieldUser />
+//                     <span>Admin Portal</span>
+//                   </Button>
+//                 </Link>
+//               </div>
+//             </div>
+//           </Card>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 "use client";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import Image from "next/image";
-import { House, ShieldUser } from "lucide-react";
-import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { LoadingSpinner } from "@/components/LoadingSpinner";
+import { EventCard } from "@/components/EventCard";
+import { Pie, PieChart } from "recharts";
+import { ChartContainer, ChartTooltip } from "@/components/ui/chart";
+import Lottie from "lottie-react";
+import searchAnimationData from "@/app/assets/search-cartoon-animation.json";
+import { BlurFade } from "@/components/magicui/blur-fade";
+import { DraggableCardDemo } from "@/components/DraggableCard";
 
 export default function Page() {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const [query, setQuery] = useState("");
+  const [events, setEvents] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [chartData, setChartData] = useState([]);
+  const [chartConfig, setChartConfig] = useState({});
+  const [totalVotes, setTotalVotes] = useState(0);
+
+  const PIE_COLORS = {
+    Good: "#82ca9d",
+    Neutral: "#ffc658",
+    Bad: "#C4554D",
+  };
+
   return (
-    <div className="flex w-full justify-center p-6 md:p-10 h-full">
-      <div className="w-full max-w-sm">
-        <div className="flex flex-col gap-4">
-          <Card className="mx-auto w-full max-w-sm p-6">
-            <div className="flex flex-col items-center text-center">
-              {/* <House size={40} /> */}
-              <Image
-                src="/paypal_logo.png"
-                width={70}
-                height={35}
-                alt="paypal icon"
-              />
-              <p className="mb-2 text-2xl font-bold">Events</p>
-              <div className="w-full flex flex-col gap-2 mt-4">
-                <Link href="/admin">
-                  <Button size={"lg"} className="w-full" variant="outline">
-                    <ShieldUser />
-                    <span>Admin Portal</span>
-                  </Button>
-                </Link>
-              </div>
-            </div>
-          </Card>
-        </div>
+    <>
+      <div className="w-full flex flex-col px-8 py-8 items-center self-center">
+        {!loading ? (
+          <LoadingSpinner className="my-10 self-center" />
+        ) : errorMessage ? (
+          <p className="text-red-500 text-sm text-center">{errorMessage}</p>
+        ) : (
+          <>
+            <DraggableCardDemo />
+          </>
+        )}
       </div>
-    </div>
+    </>
   );
 }
