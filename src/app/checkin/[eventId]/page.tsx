7e5362b8170e117f5bfcbe8609662e120d70115d
subzeroxppil <subzeroxppil@gmail.com>
2025-09-08 21:11:55 +0800

    update welcome gif

--- src/app/checkin/[eventId]/page.tsx
+++ src/app/checkin/[eventId]/page.tsx
@@ -2,7 +2,7 @@ import { CheckIn } from "@/components/CheckIn";
 
 export default function Page() {
   return (
-    <div className="flex w-full justify-center px-6 pt-6 pb-10 md:p-10">
+    <div className="flex w-full justify-center px-2 pt-6 pb-10 md:p-10">
       <div className="w-full max-w-sm">
         <CheckIn />
       </div>
