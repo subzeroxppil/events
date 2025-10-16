34e9c939d1cf5b8061a33fadd260ef30c0e08f62
subzeroxppil <subzeroxppil@gmail.com>
2025-10-16 14:23:01 +0800

    update landing page ui

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -7,19 +7,19 @@ import {
 export function DraggableCardDemo() {
   const items = [
     {
-      title: "Coming soon...",
+      title: "🔜",
       image:
         "https://images.unsplash.com/photo-1732310216648-603c0255c000?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       className: "absolute top-10 left-[30%] rotate-[-5deg]",
     },
     {
-      title: "Coming soon...",
+      title: "🔜",
       image:
         "https://images.unsplash.com/photo-1697909623564-3dae17f6c20b?q=80&w=2667&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       className: "absolute top-40 left-[35%] rotate-[-7deg]",
     },
     {
-      title: "Coming soon...",
+      title: "🔜",
       image:
         "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       className: "absolute top-5 left-[50%] rotate-[8deg]",
